aa1bfcfd26f53c671ae9d718c0277ff0dfd8a2e3
0xMCoin <0xm.coin@gmail.com>
2025-06-05 23:55:54 -0300

    fix: logo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
           name="viewport"
           content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
         />
-        <link rel="icon" href="/images/legendary.png" />
+        <link rel="icon" href="/images/contract_image.png" />
       </head>
       <body className={chakra.className}>
         <Providers>{children}</Providers>
