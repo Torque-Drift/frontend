8a68414b01f8585f5b13ebfdbeeb38472d94f619
0xMCoin <0xm.coin@gmail.com>
2025-10-26 23:50:52 -0300

    fix: mainnet

--- src/components/WalletProvider.tsx
+++ src/components/WalletProvider.tsx
@@ -7,7 +7,7 @@ import {
   darkTheme,
 } from "@rainbow-me/rainbowkit";
 import { WagmiProvider } from "wagmi";
-import { bscTestnet, bsc, hardhat } from "wagmi/chains";
+import { bsc } from "wagmi/chains";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "@/services";
 import "@rainbow-me/rainbowkit/styles.css";
@@ -15,7 +15,7 @@ import "@rainbow-me/rainbowkit/styles.css";
 const config = getDefaultConfig({
   appName: "Torque Drift",
   projectId: "development-project-id",
-  chains: [hardhat, bscTestnet, bsc],
+  chains: [bsc],
   ssr: true,
 });
 
@@ -38,3 +38,4 @@ const WalletProviderWrapper: React.FC<WalletProviderWrapperProps> = ({
 };
 
 export default WalletProviderWrapper;
+
